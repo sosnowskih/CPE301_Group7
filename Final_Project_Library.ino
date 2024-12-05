8c7bf4e1a2779945f611d19b6ce390a4de0eb2b5
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2024-12-04 21:32:04 -0800

    Digital Conversion and Serial Functions
    Converted all digital write and read to not use library functions.
    Added two serial monitor printing functions: one for a string and one for a short int variable.
    Replaced the initial time printing in the setup function to use these new functions.

--- Final_Project_Library.ino
+++ Final_Project_Library.ino
@@ -12,6 +12,17 @@ dht DHT; //Humidity/Temp Sensor
 
 RTC_DS3231 rtc; //Real Time Clock Module
 
+#define RDA 0x80
+#define TBE 0x20
+
+//Serialprint
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+
+//pin modes
 volatile unsigned char* port_a = (unsigned char*) 0x22; 
 volatile unsigned char* ddr_a = (unsigned char*) 0x21;
 volatile unsigned char* pin_a = (unsigned char*) 0x20;
@@ -26,7 +37,9 @@ volatile unsigned char* pin_e = (unsigned char*) 0x2C;
 volatile unsigned char* port_h = (unsigned char*) 0x102; 
 volatile unsigned char* ddr_h = (unsigned char*) 0x101;
 
+
 //Operation variables
+int water = 3;
 unsigned long count;
 int state = 2;
 //UI controls
@@ -34,10 +47,10 @@ const int Start = 19;
 //Temperature Sensor
 int tempThresh = 27;
 //Stepper motor driver
-int pin1 = 14; //CHANGE
-int pin2 = 15; //CHANGE
-int pin3 = 16; //CHANGE
-int pin4 = 17; //CHANGE
+int pin1 = 14; 
+int pin2 = 15; 
+int pin3 = 16; 
+int pin4 = 17; 
 const int stepsPerRevolution = 2048;
 int tracker = 0;
 int begin = 0;
@@ -53,16 +66,20 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 Stepper myStepper = Stepper(stepsPerRevolution, pin1, pin3, pin2, pin4);
 
 void setup() {
-  Serial.begin(9600); //Serial Monitor Setup
-  Serial.println("Serial Monitor Ready");
+  U0init(9600);
+  Serial.begin(9600); //Serial Monitor Setup  //CHANGE
+  Serialstring("Serial Monitor Ready");
+  U0putchar('\n');
 
   attachInterrupt(digitalPinToInterrupt(Start), pin_ISR, RISING); //Setting inturrupt for Start button 
 
-  //Pinmodes for all components
+  //Pin modes for all components
   *ddr_a &= 0xFE; //Sets 22 PA0 to input for Stop Button
   *port_a |= 0x01;
   *ddr_a &= 0xFD; //Sets 23 PA1 to input for Reset Button
-  *port_a |= 0x2;
+  *port_a |= 0x02;
+  *ddr_d &= 0xF7;//Sets 18 PD3 to input for Vent Button
+  *port_d |= 0x08;
   *ddr_e |= 0x10;  //Sets 2 PE4 to output for Red LED
   *ddr_h |= 0x40; //Sets 9 PH6 to output for Yellow LED
   *ddr_a |= 0x04;  //Sets 24 PA2 to output for Green LED
@@ -79,19 +96,19 @@ void setup() {
   rtc.begin();
   
   DateTime now = rtc.now(); //Initial Clock Output mm/dd/yyyy "day of the week" hh:mm:ss
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print('/');
-  Serial.print(now.year(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.println(now.second(), DEC);
+  
+  Serialint(now.month());
+  Serialstring("/");
+  Serialint(now.day());
+  Serialstring("/");
+  Serialint(now.year());
+  Serialstring(" ");
+  Serialint(now.hour());
+  Serialstring(":");
+  Serialint(now.minute());
+  Serialstring(":");
+  Serialint(now.second());
+  Serialstring("\n");
 
   lcd.begin(16,2); //LCD columns and rows
 }
@@ -119,7 +136,7 @@ void loop() {
     count = millis(); //updates "count" value for next iteration
   } 
 
-  if((state != 0) && (digitalRead(ventButton) == 1)) { //Vent control loop //CHANGE
+  if((state != 0) && (*pin_d & 0x08)) { //Vent control loop
     myStepper.setSpeed(10); //speed set to 10 rpm
     myStepper.step(stepsPerRevolution/10); //rotate a tenth of a rotation every time
     lcd.setCursor(5,1);
@@ -147,83 +164,83 @@ void loop() {
   }
   
   if(state == 1) { //IDLE STATE
-    digitalWrite(GreenLED, HIGH); //CHANGE
+    *port_a |= 0x04; //green LED high
     lcd.setCursor(12,0);
     lcd.print("IDLE"); //prints "IDLE" to top right corner of lcd screen
     if((DHT.temperature > tempThresh)) { //if the temp is too high, switch to running
       state = 3;
-      digitalWrite(GreenLED, LOW); //CHANGE
-      digitalWrite(BlueLED, HIGH); //adjust to running lights //CHANGE
+      *port_a &= 0xFB; //green LED low
+      *port_a |= 0x08; //blue LED high //adjust to running lights
       lcd.clear();
       lcd.setCursor(9,0);
       lcd.print("RUNNING"); //print state to lcd screen
       state_trans(); //run state transition code
     } else if(water_Level() < 100) { //if the water is too low, throw an error
       state = 2;
-      digitalWrite(GreenLED, LOW); //CHANGE
-      digitalWrite(RedLED, HIGH); //adjust to error lights //CHANGE
+      *port_a &= 0xFB; //green LED low
+      *port_e |= 0x10; // red LED high //adjust to error lights
       lcd.clear();
       lcd.setCursor(11,0);
       lcd.print("ERROR"); //print state to lcd screen
       state_trans(); //run state transition code
-    } else if(digitalRead(Stop) == 1) { //if "stop" is pressed, disable system //CHANGE
+    } else if(*pin_a & 0x01) { //if "stop" is pressed, disable system
       state = 0;
-      digitalWrite(GreenLED, LOW); //CHANGE
-      digitalWrite(YellowLED, HIGH); //adjust to "stop" lights //CHANGE
+      *port_a &= 0xFB; //green LED low
+      *port_h |= 0x40; //yellow LED high //adjust to "stop" lights
       lcd.clear();
       state_trans(); //run state transition code
     }
 
   } else if(state == 2) { //ERROR STATE
-    if(digitalRead(Reset) == 1) { //switch to idle if "reset" is pressed //CHANGE
+    if(*pin_a & 0x02) { //switch to idle if "reset" is pressed
       state = 1;
-      digitalWrite(RedLED, LOW); //CHANGE
-      digitalWrite(GreenLED, HIGH); //change lights to idle state //CHANGE
+      *port_e &= 0xEF; // red LED low
+      *port_a |= 0x04;//green LED high //change lights to idle state 
       lcd.clear();
       lcd.setCursor(12,0);
       lcd.print("IDLE"); //print "IDLE" to top right corner of lcd
       state_trans(); //run state transition code
     } else { //otherwise, run error as normal
-      digitalWrite(RedLED, HIGH); //CHANGE
+      *port_e |= 0x10; // red LED high
       lcd.setCursor(11,0);
       lcd.print("ERROR"); //print "ERROR" to top right corner of lcd
     }
 
   } else if(state == 3) { //RUNNING STATE
-    if(digitalRead(Stop) == 1) { //disable if "stop" is pressed //CHANGE
+    if(*pin_a & 0x01) { //disable if "stop" is pressed
       state = 0;
-      digitalWrite(YellowLED, HIGH); //switch to stop lights //CHANGE
-      digitalWrite(BlueLED, LOW); //CHANGE
-      digitalWrite(DCmotor, LOW); //turn off fan motor //CHANGE
+      *port_h |= 0x40; //yellow LED high //switch to stop lights
+      *port_a &= 0xF7; //blue LED low
+      *port_a &= 0x7F; //DC motor low //turn off fan motor
       lcd.clear();
       state_trans(); //run state transition code
     } else if(DHT.temperature <= tempThresh) { //if temp drops to normal, switch to idle
       state = 1;
-      digitalWrite(GreenLED, HIGH); //switch to "IDLE" lights //CHANGE
-      digitalWrite(BlueLED, LOW); //CHANGE
-      digitalWrite(DCmotor, LOW); //turn off fan motor //CHANGE
+      *port_a |= 0x04; //green LED high //switch to "IDLE" lights
+      *port_a &= 0xF7; //blue LED low
+      *port_a &= 0x7F; //DC motor low //turn off fan motor
       lcd.clear();
       lcd.setCursor(12,0);
       lcd.print("IDLE"); //print idle to top right corner of lcd
       state_trans(); //run state transition code
     } else if(water_Level() < 100) { //if water level drops too low, switch to error
       state = 2;
-      digitalWrite(BlueLED, LOW); //CHANGE
-      digitalWrite(RedLED, HIGH); //switch to error lights //CHANGE
-      digitalWrite(DCmotor, LOW); //turn off fan motor //CHANGE
+      *port_a &= 0xF7; //blue LED low
+      *port_e |= 0x10; // red LED high //switch to error lights
+      *port_a &= 0x7F; //DC motor low //turn off fan motor
       lcd.clear();
       lcd.setCursor(11,0);
       lcd.print("ERROR"); //print "ERROR" to top right corner of lcd
       state_trans(); //run state transition code
     } else { //"RUNNING" state
-      digitalWrite(BlueLED, HIGH); //CHANGE
-      digitalWrite(DCmotor, HIGH); //CHANGE
+      *port_a |= 0x08; //blue LED high
+      *port_a |= 0x80; //DC motor high
       lcd.setCursor(9,0);
       lcd.print("RUNNING"); //print "RUNNING" to top right corner of lcd
     }
 
   } else if(state == 0) { //DISABLED STARE
-    digitalWrite(YellowLED, HIGH); //yellow light is on for diabled //CHANGE
+    *port_h |= 0x40; //yellow LED high //yellow light is on for diabled
     lcd.setCursor(4,0);
     lcd.print("DISABLED"); // only print "DISABLED" on center of lcd
   }
@@ -232,8 +249,8 @@ void loop() {
 void pin_ISR() { //INTERRUPT for "START" button
   if(state == 0) {
     state = 1;
-    digitalWrite(YellowLED, LOW); //turn off diabled light //CHANGE
-    digitalWrite(GreenLED, HIGH); //turn on IDLE light
+    *port_h &= 0xBF; //yellow LEF low //turn off diabled light
+    *port_a |= 0x04; //green LED high //turn on IDLE light
     begin = 1;
   }
 }
@@ -262,4 +279,50 @@ int state_trans() {
     Serial.println("STATE: RUNNING");
     Serial.println("");
       }
+}
+
+//initializes the serial monitor
+void U0init(int U0baud)
+{
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+
+//prints an int between 1 to 5 digits long to the serial monitor 
+void Serialint(int input){
+  int length = 1;
+  int inputl = input;
+  for (int i = 0; inputl/10 !=0; i++){
+    length += 1;
+    inputl = inputl/10;
+  }
+  int print[length] = {0};
+  for (int n=1; n <= length; n++){
+    print[length-n] = input%10;
+    input = input/10;
+  }
+  for (int n=0; n < length; n++){
+    while((*myUCSR0A & TBE)==0);
+    *myUDR0 = print[n] + '0';
+  }
+}
+
+//prints a char array to the serial monitor
+void Serialstring(unsigned char input[]) {
+  for (int i=0; input[i]; i++){
+    while((*myUCSR0A & TBE)==0);
+    *myUDR0 = input[i];
+  }
+}
+
+//prints a char to the serial monitor
+void U0putchar(unsigned char U0pdata)
+{
+  while((*myUCSR0A & TBE) == 0);
+    *myUDR0 = U0pdata;
 }
